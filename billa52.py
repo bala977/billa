10c2b689575f4507a9de7cd1cd92cc0d8d143f71
Selvakumar030998 <52271089+Selvakumar030998@users.noreply.github.com>
2019-07-16 21:38:23 -0700

    Update billa52.py

--- billa52.py
+++ billa52.py
@@ -1,23 +1,4 @@
-z111=int(input())
-	if z111==1:
-	    print("One")
-	elif z111==2:
-	    print("Two")
-	elif z111==3:
-	    print("Three")
-	elif z111==4:
-	    print("Four")
-	elif z111==5:
-	    print("Five")
-	elif z111==6:
-	    print("Six")
-	elif z111==7:
-	    print("seven")
-	elif z111==8:
-	    print("Eight")
-	elif z111==9:
-	    print("Nine")
-	elif z111==10:
-	    print("Ten")
-	else:
-	    print("no")
+NIn=['One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
+PA=int(input())
+if 1<=PA<=10:
+  print(NIn[PA-1])
